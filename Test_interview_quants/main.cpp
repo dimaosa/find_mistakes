8a33cd15ce0d5c3bbd9c4be077240b23cff40fee
Dima Osadchy <dima.osadchy@gmail.com>
2015-03-03 14:33:04 +0200

    Version without mistakes

--- Test_interview_quants/main.cpp
+++ Test_interview_quants/main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 
 int swap_counts;
 
-void sorting(vector<double>* Array, int& first, int& last)
-{
-    if (first < 0 || last < 0)
+void sorting(std::vector<double>& array, int first, int last){
+    
+    if (first < 0 || last < 0){
         return;
+    }
     
     int i = first, j = last;
-    double x = Array[(first + last) / 2];
+    double x = array[(first + last) / 2];
     do
     {
-        while (Array[i]<x) i++;
-        while (Array[j]>x) j--;
+        while (array[i]<x) i++;
+        while (array[j]>x) j--;
         
         if (i <= j)
         {
-            if (Array[i]>Array[j])
+            if (array[i]>array[j])
             {
-                Array[i] = Array[j];
-                Array[j] = Array[i];
+                double temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
                 swap_counts++;
             }
             i++;
@@ -29,31 +32,45 @@ void sorting(vector<double>* Array, int& first, int& last)
     } while (i <= j);
     
     if (i<last)
-        sorting(Array, i, last);
+        sorting(array, i, last);
     if (first<j)
-        sorting(Array, first, j);
+        sorting(array, first, j);
 }
 
-int main()
-{
-    srand((unsigned int)time(0));
+void generate_array(std::vector<double>& arr, int size, double rand_min, double rand_max){
+    srand((unsigned int)time(NULL));
     
-    int n;
-    vector<double> arr;
-    cout << "Enter n: ";
-    cin >> n;
-    
-    for (int i = n; i >= 0; i++)
-    {
-        arr[i] = (double)i / double(rand() % n);
-        cout << arr[i] << " ";
+    for (int i = 0; i < size; ++i){
+        arr.push_back(rand_min + ((double)rand() / RAND_MAX) * (rand_max - rand_min));
     }
+
+}
+void show_array(const std::vector<double>& arr){
+    for(auto elem : arr){
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main(int argc, char* argv[]){
+    int arr_size;
+    std::vector<double> arr;
+    std::cout << "Enter array size: ";
+    std::cin >> arr_size;
+    std::cout << std::endl;
     
-    cout << endl;
-    sorting(arr, 1, n);
+    //generate array
+    double rand_min = -23.45;
+    double rand_max = 45.954;
+    generate_array(arr, arr_size, rand_min, rand_max);
+    show_array(arr);
     
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
+    //quicksort
+    sorting(arr, 0, arr_size);
     
-    cout << endl << "Swaps: " << swap_counts;
+    //result of sorted array
+    show_array(arr);
+    
+    std::cout << std::endl << "Swaps: " << swap_counts << std::endl;
 }
